8ca65e175d724752ec9524f0f3d09d8730fc480e
Jared Hanson <jaredhanson@gmail.com>
2013-02-15 16:37:37 -0800

    Move rendering implementation to main file.

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 define(['exports',
-        './lib/context'],
-function(exports, context) {
-  exports.$ = context.$;
-  exports.render = context.render;
+        'dom',
+        './lib/dom/render'],
+function(exports, DOM, Render) {
+  
+  DOM.augment(Render);
+  
+  exports.render = function(name, options) {
+    if (typeof name == 'function') {
+      render = name;
+      return;
+    }
+    return render.apply(undefined, arguments);
+  };
+  
+  exports.$ = function(nodes) {
+    if (typeof nodes == 'function') {
+      wrap = nodes;
+      return;
+    }
+    return wrap.apply(undefined, arguments);
+  }
+  
+  
+  var render = function(name, options) {
+    throw new Error('Invalid context: sail.render() must be implemented by application');
+  }
+  var wrap = DOM;
 });
